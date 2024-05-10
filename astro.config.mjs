a772a775419baa1b79f866a6e6de79bd4105f72a
Alex Chiu <alexchiu11@gmail.com>
2024-05-10 01:59:35 +0100

    chore: install react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "hybrid",
+  // or 'server'
+  experimental: {
+    actions: true
+  },
+  integrations: [react()]
+});
