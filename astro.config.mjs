0af36d994b02ad01b6e3eb722aeb4dcebc350260
Alex Chiu <alexchiu11@gmail.com>
2024-05-12 04:29:38 +0100

    install cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   experimental: {
-    actions: true,
+    actions: true
   },
   integrations: [react()],
+  adapter: cloudflare()
 });
