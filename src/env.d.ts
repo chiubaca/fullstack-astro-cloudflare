95cc65884fe6a935e97a49f3491e0ded79a860e6
Alex Chiu <alexchiu11@gmail.com>
2024-05-19 19:14:48 +0100

    feat: implement lucia github oauth (#1)

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,23 @@
 /// <reference path="../.astro/actions.d.ts" />
 /// <reference types="astro/client" />
+/// <reference types="lucia" />
 
 type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 declare namespace App {
   interface Locals extends Runtime {}
+}
+
+type User = import("lucia").User;
+
+interface GitHubUser extends User {
+  github_id?: string;
+  username?: string;
+}
+
+/// <reference types="astro/client" />
+declare namespace App {
+  interface Locals {
+    session: import("lucia").Session | null;
+    user: GitHubUser | null;
+  }
 }
