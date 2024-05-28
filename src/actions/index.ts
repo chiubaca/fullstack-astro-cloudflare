dba7cfe1e6618bf4842589966fa1c8d09325d967
Alex Chiu <alexander.chiu@houseful.co.uk>
2024-05-28 17:32:37 +0100

    fix: tidy up action

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -13,31 +13,25 @@ export const server = {
       imageFile: z.instanceof(File).optional(),
     }),
     handler: async (input, context) => {
-      console.log("🚀 ~ handler: ~ input:", input);
       const { locals } = context;
       const APP_DB = locals.runtime.env.APP_DB;
 
       let imageRef: string | undefined;
 
-      // if image is submitted we need to store to r2
-      if (input.imageFile) {
-        console.log("🚀 ~ r2 upload!");
+      if (input.imageFile && input.imageFile.type.startsWith("image")) {
         const { putObject } = bucketAccess(context.locals.runtime.env);
 
         try {
-          //convert input.imageFile into a buffer
           const fileBuffer = await input.imageFile.arrayBuffer();
           imageRef = nanoid(10) + "_" + input.imageFile.name;
 
-          const r2Resp = await putObject({
+          await putObject({
             key: imageRef,
             body: fileBuffer as any,
             contentType: input.imageFile.type,
           });
-
-          console.log("🚀 ~ handler: ~ r2Resp:", r2Resp);
         } catch (error) {
-          console.warn("Internal error uploading to bucket", error);
+          console.warn("Unhandled error uploading to bucket", error);
         }
       }
 
@@ -48,28 +42,6 @@ export const server = {
         .returning();
 
       return { data: resp[0] };
-    },
-  }),
-
-  fileUpload: defineAction({
-    accept: "form",
-    input: z.object({
-      imageFile: z.instanceof(File),
-    }),
-    handler: async (input, context) => {
-      console.log("🚀 ~ handler: ~ input:", input);
-
-      const { locals } = context;
-      const r2Bucket = locals.runtime.env.APP_BUCKET;
-
-      const resp = await r2Bucket.put("local-image", input.imageFile, {
-        httpMetadata: {
-          contentType: input.imageFile.type,
-        },
-      });
-      console.log("🚀 ~ handler: ~ resp:", resp);
-
-      return "success!";
     },
   }),
 };
