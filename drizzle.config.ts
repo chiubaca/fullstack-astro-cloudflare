c76ef4193619e94e6ea2575965ff9bbaa1b0fc34
Alex Chiu <alexander.chiu@houseful.co.uk>
2024-06-09 17:25:14 +0100

    tidyup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,16 +6,6 @@ const config = defineConfig({
   verbose: true,
   strict: true,
   driver: "d1-http",
-  // dbCredentials:{
-  //   wranglerConfigPath: "./wrangler.toml",
-  //   dbName: 'fullstack-astro-cloudflare-db'
-  // },
-  dbCredentials: {
-    accountId: "6fba426cae95d09cbd43f7d359f87924",
-    databaseId: "307c117f-ab77-485c-a26e-deb8586cbd3b",
-    token: "T8xfT6Gppkkd2UXC7g_qVeP3im6ajLQs3rJRmT2H",
-  },
 });
 
-
-export default config
+export default config;
